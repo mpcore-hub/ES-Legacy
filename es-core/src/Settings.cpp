c5324eb6e117ce2245c8ed73f531ec872627b29c
RetrOrangePi <retrorangepi@outlook.com>
2018-01-25 00:17:54 +0000

    Fixing unset DebugImage! error

--- es-core/src/Settings.cpp
+++ es-core/src/Settings.cpp
@@ -73,6 +73,7 @@ void Settings::setDefaults()
 	mBoolMap["Debug"] = false;
 	mBoolMap["DebugGrid"] = false;
 	mBoolMap["DebugText"] = false;
+	mBoolMap["DebugImage"] = false;
 
 	mIntMap["ScreenSaverTime"] = 5*60*1000; // 5 minutes
 	mIntMap["ScraperResizeWidth"] = 400;
