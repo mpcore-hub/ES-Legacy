198e793c0cf906f4a05ddfdbe40287fe305bf4d6
microplay <48363872+microplay@users.noreply.github.com>
2019-03-09 09:28:49 +0100

    282rp

--- es-app/src/EmulationStation.h
+++ es-app/src/EmulationStation.h
@@ -6,12 +6,12 @@
 // Do this version number update as the very last commit for the new release version.
 #define PROGRAM_VERSION_MAJOR       2
 #define PROGRAM_VERSION_MINOR        8
-#define PROGRAM_VERSION_MAINTENANCE  0
-#define PROGRAM_VERSION_STRING "2.8.0rp - RetrOrangePi 4.2"
+#define PROGRAM_VERSION_MAINTENANCE  2
+#define PROGRAM_VERSION_STRING "2.8.2rp - mpcore"
 
 #define PROGRAM_BUILT_STRING __DATE__ " - " __TIME__
 
-#define RESOURCE_VERSION_STRING "2,8,0\0"
+#define RESOURCE_VERSION_STRING "2,8,2\0"
 #define RESOURCE_VERSION PROGRAM_VERSION_MAJOR,PROGRAM_VERSION_MINOR,PROGRAM_VERSION_MAINTENANCE
 
 #endif // ES_APP_EMULATION_STATION_H
