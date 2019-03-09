8d4cfb84f26578a06a3694ef010f45abd4997769
microplay <48363872+microplay@users.noreply.github.com>
2019-03-09 13:19:51 +0100

    Fix for unset AudioCard setting

--- es-core/src/Settings.cpp
+++ es-core/src/Settings.cpp
@@ -136,6 +136,7 @@ void Settings::setDefaults()
 		mStringMap["AudioDevice"] = "Master";
 	#endif
 
+	mStringMap["AudioCard"] = "default";
 	mStringMap["UIMode"] = "Full";
 	mStringMap["UIMode_passkey"] = "uuddlrlrba";
 	mBoolMap["ForceKiosk"] = false;
