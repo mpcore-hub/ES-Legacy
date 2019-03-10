72b48fad917c9c39ee87f3b928c7e20ddb37c056
microplay <48363872+microplay@users.noreply.github.com>
2019-03-10 09:46:48 +0100

    loading state dev 2.9

--- es-app/src/main.cpp
+++ es-app/src/main.cpp
@@ -291,7 +291,10 @@ int main(int argc, char* argv[])
 	CollectionSystemManager::init(&window);
 	MameNames::init();
 	window.pushGui(ViewController::get());
-
+	
+	bool splashScreen = Settings::getInstance()->getBool("SplashScreen");
+	bool splashScreenProgress = Settings::getInstance()->getBool("SplashScreenProgress");
+	
 	if(!scrape_cmdline)
 	{
 		if(!window.init())
@@ -303,8 +306,13 @@ int main(int argc, char* argv[])
 		std::string glExts = (const char*)glGetString(GL_EXTENSIONS);
 		LOG(LogInfo) << "Checking available OpenGL extensions...";
 		LOG(LogInfo) << " ARB_texture_non_power_of_two: " << (glExts.find("ARB_texture_non_power_of_two") != std::string::npos ? "ok" : "MISSING");
-		if(Settings::getInstance()->getBool("SplashScreen"))
-			window.renderLoadingScreen();
+		if(splashScreen)
+		{
+			std::string progressText = "Loading...";
+			if (splashScreenProgress)
+				progressText = "Loading system config...";
+			window.renderLoadingScreen(progressText);
+		}
 	}
 
 	const char* errorMsg = NULL;
@@ -342,6 +350,9 @@ int main(int argc, char* argv[])
 	// this makes for no delays when accessing content, but a longer startup time
 	ViewController::get()->preload();
 
+	if(splashScreen && splashScreenProgress)
+		window.renderLoadingScreen("Done.");
+	
 	//choose which GUI to open depending on if an input configuration already exists
 	if(errorMsg == NULL)
 	{
