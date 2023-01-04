6191353a516137d746d8f9f2e1f1baa127e96d5b
Fredy <44968046+Liontek1985@users.noreply.github.com>
2023-01-04 16:53:24 +0100

    Grid View Fixes

--- es-app/src/views/gamelist/GridGameListView.cpp
+++ es-app/src/views/gamelist/GridGameListView.cpp
@@ -78,6 +78,11 @@ GridGameListView::GridGameListView(Window* window, FileData* root) :
 	updateInfoPanel();
 }
 
+GridGameListView::~GridGameListView()
+{
+	delete mVideo;
+}
+
 FileData* GridGameListView::getCursor()
 {
 	return mGrid.getSelected();
@@ -179,8 +184,11 @@ void GridGameListView::onThemeChanged(const std::shared_ptr<ThemeData>& theme)
 	mDescription.setSize(mDescContainer.getSize().x(), 0);
 	mDescription.applyTheme(theme, getName(), "md_description", ALL ^ (POSITION | ThemeFlags::SIZE | ThemeFlags::ORIGIN | TEXT | ROTATION));
 
+	// Repopulate list in case new theme is displaying a different image.  Preserve selection.
+	FileData* file = mGrid.getSelected();
 	populateList(mRoot->getChildrenListToDisplay());
-
+	mGrid.setCursor(file);
+	
 	sortChildren();
 }
 
@@ -392,4 +400,16 @@ std::vector<HelpPrompt> GridGameListView::getHelpPrompts()
 		prompts.push_back(HelpPrompt("y", prompt));
 	}
 	return prompts;
+}
+
+void GridGameListView::update(int deltaTime)
+{
+	ISimpleGameListView::update(deltaTime);
+	mVideo->update(deltaTime);
+}
+
+void GridGameListView::onShow()
+{
+	GuiComponent::onShow();
+	updateInfoPanel();
 }
