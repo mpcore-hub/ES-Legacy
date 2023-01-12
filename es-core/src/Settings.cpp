3bdf206a039c12c5c24b1f16224f6777856e9a38
Fredy <44968046+Liontek1985@users.noreply.github.com>
2023-01-12 08:36:25 +0100

    Add option to ignore common leading articles in Filename sort

--- es-core/src/Settings.cpp
+++ es-core/src/Settings.cpp
@@ -156,6 +156,11 @@ void Settings::setDefaults()
 	mIntMap["ScreenRotate"]  = 0;
 	
 	mBoolMap["UseFullscreenPaging"] = false;
+
+	mBoolMap["IgnoreLeadingArticles"] = false;
+	//No spaces!  Order is important!
+	//"The A Squad" given [a,an,the] will sort as "A Squad", but given [the,a,an] will sort as "Squad"
+	mStringMap["LeadingArticles"] = "a,an,the";
 }
 
 template <typename K, typename V>
